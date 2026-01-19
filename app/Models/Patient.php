ed55e5980ba7c73d4e9056757cdd41f458898516
Belén Echechury <bechechury@sightly.com>
2026-01-19 03:53:09 -0300

    Patient model and migration

---
+++ app/Models/Patient.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'country_iso',
+        'phone_number',
+        'document_image_path',
+    ];
+}
